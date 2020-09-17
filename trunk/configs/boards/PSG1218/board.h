ac74f098553cb75631a559010051522aebfaf5a0
natserver <55800291+natserver@users.noreply.github.com>
2020-09-17 19:59:02 +0800

    Update board.h

--- trunk/configs/boards/PSG1218/board.h
+++ trunk/configs/boards/PSG1218/board.h
@@ -1,8 +1,8 @@
 /* PSG1218 */
 
-#define BOARD_PID		"PSG1218"
-#define BOARD_NAME		"PSG1218"
-#define BOARD_DESC		"PHICOMM PSG1218 Wireless Router"
+#define BOARD_PID		"S-LINK"
+#define BOARD_NAME		"S-LINK"
+#define BOARD_DESC		"PHICOMM S-LINK Wireless Router"
 #define BOARD_VENDOR_NAME	"PHICOMM"
 #define BOARD_VENDOR_URL	"http://www.phicomm.com/"
 #define BOARD_MODEL_URL		"http://www.phicomm.com/"
